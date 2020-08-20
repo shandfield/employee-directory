85b55b419d5c26c4115889d3a6d8a47f27ac3876
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-08-20 15:08:53 -0600

    added in code to result list to edit table to figure out display data

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -3,22 +3,27 @@ import React from "react";
 function ResultList(props) {
   return (
     <table className="table">
-      {props.results.map(result => (
-        <table style="width:100%">
+      <thead>
         <tr>
-          <th>Firstname</th>
-          <th>Lastname</th>
-          <th>Age</th>
+          <th></th>
+          <th OnClick = {props.sortByName}>Name</th>
+          <th>Phone</th>
+          <th>Email</th>
+          <th>DOB</th>
         </tr>
-        <tr>
-          <td>Jill</td >
-          <td>Smith</td>
-          <td>50</td>
-        </tr>
-      </table>
-      ))}
-    </table>
-  );
-}
+      </thead>
 
+      <tbody className = "">
+      {props.results.map(result => (
+        <tr className= "table">
+          <td><img className= "" src= {result.picture.medium} alt= "" /></td>
+          <td>{result.name.first + " " + result.name.last}</td>
+          <td> {result.cell}</td>
+          <td className="email"><a href={result.email}>{result.email}</a></td>
+        </tr>
+      ))}
+      </tbody>
+    </table>
+  )
+};
 export default ResultList;
