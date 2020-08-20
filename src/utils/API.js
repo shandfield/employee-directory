21995684a173563dc56d0fba22459f3490ad55bf
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-08-20 13:54:22 -0600

    put in code to connect to the random user api to have data for this employee search

---
+++ src/utils/API.js
@@ -0,0 +1,10 @@
+//using a random user generator API for data 
+import axios from "axios";
+const BASEURL = "https://randomuser.me/api/?results=50&nat=us";
+
+
+export default {
+  search: function() {
+    return axios.get(BASEURL);
+  }
+};
